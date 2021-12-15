bbd30f4377137be6428ca66258adb1927bbb6e91
hyun2 <hks0284@gmail.com>
2021-12-15 11:03:44 +0900

    토큰 전송 시 화면에서 바로 반영하도록 구현

--- src/App.js
+++ src/App.js
@@ -1,90 +1,93 @@
-import { useEffect, useState } from 'react';
-import erc721Abi from './erc721Abi.js';
-import _ from 'lodash';
-import Web3 from 'web3';
+import { useEffect, useState } from "react";
+import erc721Abi from "./erc721Abi.js";
+import _ from "lodash";
+import Web3 from "web3";
 
-import TokenList from './components/TokenList';
+import TokenList from "./components/TokenList";
 
 function App() {
-	const [web3, setWeb3] = useState();
-	const [account, setAccount] = useState('');
-	const [newErc721addr, setNewErc721addr] = useState('');
-	const [erc721list, setErc721list] = useState([]);
+  const [web3, setWeb3] = useState();
+  const [account, setAccount] = useState("");
+  const [newErc721addr, setNewErc721addr] = useState("");
+  const [erc721list, setErc721list] = useState([]);
 
-	useEffect(() => {
-		if (typeof window.ethereum !== 'undefined') {
-			try {
-				const web = new Web3(window.ethereum);
-				setWeb3(web);
-			} catch (err) {
-				console.log(err);
-			}
-		}
-	}, []);
-	const connectWallet = async () => {
-		const accounts = await window.ethereum.request({
-			method: 'eth_requestAccounts',
-		});
-		setAccount(accounts[0]);
-	};
+  useEffect(() => {
+    if (typeof window.ethereum !== "undefined") {
+      try {
+        const web = new Web3(window.ethereum);
+        setWeb3(web);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }, []);
+  const connectWallet = async () => {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    setAccount(accounts[0]);
+  };
 
-	const addNewErc721Token = async () => {
-		console.log('addNewErc called');
-		try {
-			const tokenContract = await new web3.eth.Contract(
-				erc721Abi,
-				newErc721addr
-			);
-			const name = await tokenContract.methods.name().call();
-			const symbol = await tokenContract.methods.symbol().call();
-			const totalSupply = await tokenContract.methods.totalSupply().call();
-			// token id arr
-			let arr = [];
-			// 비교할 token 객체 arr
-			let tokens = [];
-			for (let i = 1; i <= totalSupply; i++) {
-				arr.push(i);
-			}
-			for (let tokenId of arr) {
-				let tokenOwner = await tokenContract.methods.ownerOf(tokenId).call();
-				if (String(tokenOwner).toLowerCase() === account) {
-					let tokenURI = await tokenContract.methods.tokenURI(tokenId).call();
-					tokens.push({ name, symbol, tokenId, tokenURI });
-				}
-			}
-			// tokens + erc721list 후 tokenId로 중복제거
-			let uniqArr = _.uniqBy([...erc721list, ...tokens], 'tokenId');
-			setErc721list(uniqArr);
-		} catch (error) {
-			alert(error);
-		}
-	};
-	return (
-		<div className='App'>
-			<button
-				className='metaConnect'
-				onClick={() => {
-					connectWallet();
-				}}>
-				connect to MetaMast
-			</button>
-			<div className='userInfo'>주소: {account}</div>
-			<div className='newErc721'>
-				<input
-					type='text'
-					onChange={(e) => {
-						setNewErc721addr(e.target.value); // 입력받을 때마다 newErc721addr 갱신
-					}}></input>
-				<button onClick={addNewErc721Token}>add new erc721</button>
-			</div>
-			<TokenList
-				web3={web3}
-				account={account}
-				erc721list={erc721list}
-				newErc721addr={newErc721addr}
-			/>
-		</div>
-	);
+  const addNewErc721Token = async () => {
+    console.log("addNewErc called");
+    try {
+      const tokenContract = await new web3.eth.Contract(
+        erc721Abi,
+        newErc721addr
+      );
+      const name = await tokenContract.methods.name().call();
+      const symbol = await tokenContract.methods.symbol().call();
+      const totalSupply = await tokenContract.methods.totalSupply().call();
+      // token id arr
+      let arr = [];
+      // 비교할 token 객체 arr
+      let tokens = [];
+      for (let i = 1; i <= totalSupply; i++) {
+        arr.push(i);
+      }
+      for (let tokenId of arr) {
+        let tokenOwner = await tokenContract.methods.ownerOf(tokenId).call();
+        if (String(tokenOwner).toLowerCase() === account) {
+          let tokenURI = await tokenContract.methods.tokenURI(tokenId).call();
+          tokens.push({ name, symbol, tokenId, tokenURI });
+        }
+      }
+      // tokens + erc721list 후 tokenId로 중복제거
+      let uniqArr = _.uniqBy([...erc721list, ...tokens], "tokenId");
+      setErc721list(uniqArr);
+    } catch (error) {
+      alert(error);
+    }
+  };
+  return (
+    <div className="App">
+      <button
+        className="metaConnect"
+        onClick={() => {
+          connectWallet();
+        }}
+      >
+        connect to MetaMast
+      </button>
+      <div className="userInfo">주소: {account}</div>
+      <div className="newErc721">
+        <input
+          type="text"
+          onChange={(e) => {
+            setNewErc721addr(e.target.value); // 입력받을 때마다 newErc721addr 갱신
+          }}
+        ></input>
+        <button onClick={addNewErc721Token}>add new erc721</button>
+      </div>
+      <TokenList
+        web3={web3}
+        account={account}
+        erc721list={erc721list}
+        newErc721addr={newErc721addr}
+        setErc721list={setErc721list}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/components/TokenList.js
+++ src/components/TokenList.js
@@ -1,17 +1,24 @@
-import Erc721 from './Erc721';
+import Erc721 from "./Erc721";
 
 // TokenList.js
-function TokenList({ web3, account, erc721list, newErc721addr }) {
-	return (
-		<div className='tokenlist'>
-			<Erc721
-				web3={web3}
-				account={account}
-				erc721list={erc721list}
-				erc721addr={newErc721addr}
-			/>
-		</div>
-	);
+function TokenList({
+  web3,
+  account,
+  erc721list,
+  newErc721addr,
+  setErc721list,
+}) {
+  return (
+    <div className="tokenlist">
+      <Erc721
+        web3={web3}
+        account={account}
+        erc721list={erc721list}
+        erc721addr={newErc721addr}
+        setErc721list={setErc721list}
+      />
+    </div>
+  );
 }
 
 export default TokenList;
