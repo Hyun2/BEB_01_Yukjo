dac764826610e57e0c4bc5c319ddda90e018bbc8
dongmoon <ehdans04@gmail.com>
2021-12-15 16:14:09 +0900

    done: add etherscan search bar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Header.css';
 
 function Header() {
+	const [searchInput, setSearchInput] = useState('');
+
+	const changeInputHandler = (e) => {
+		setSearchInput(e.target.value);
+	};
+	const clickClearSearchInputHandler = () => {
+		setSearchInput('');
+	};
+
 	return (
 		<div className='header'>
 			<div className='header__logo'>
 				<img src='img/opensea.svg' alt='' className='header__logo__img' />
 				<h3 className='header__logo__title'>YukJO</h3>
 			</div>
+			<div className='search_box'>
+				<a
+					className='search_box__btn'
+					href={`https://etherscan.io/address/${searchInput}`}
+					target='_blank'
+					rel='noopener noreferrer'>
+					<i className='fas fa-search'></i>
+				</a>
+				<input
+					className='search_box__input'
+					type='text'
+					placeholder='contract address, wallet address....'
+					onChange={changeInputHandler}
+					value={searchInput}
+				/>
+				<button
+					className='search_box__remove_btn'
+					onClick={clickClearSearchInputHandler}>
+					<i className='fas fa-backspace'></i>
+				</button>
+			</div>
+
 			<div className='header__account'>
 				<div className='header__account__user-icon'>
 					<i className='fas fa-user'></i>
