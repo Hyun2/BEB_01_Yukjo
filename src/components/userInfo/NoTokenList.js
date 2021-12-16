dddca84432624944244ca43903101123338bd298
dongmoon <ehdans04@gmail.com>
2021-12-16 11:12:43 +0900

    NoTokenList component

--- src/components/userInfo/NoTokenList.js
+++ src/components/userInfo/NoTokenList.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './NoTokenList.css';
 function NoTokenList() {
-	return <div>No TokenList</div>;
+	const [selectedTokenType, setSelectedTokenType] = useState('');
+
+	const selectTokenType = (tokenType) => {
+		setSelectedTokenType(tokenType);
+	};
+
+	return (
+		<div className='card--no-token'>
+			<div className='add--token--addr--container'>
+				<div className='add--token--addr--token__type'>
+					<h3>New ERC 20 Address</h3>
+					<i
+						className='fas fa-donate'
+						onClick={() => selectTokenType('erc20')}></i>
+				</div>
+				<div className='add--token--addr--token__type'>
+					<h3>New ERC 721 Address</h3>
+					<i
+						className='fas fa-donate'
+						onClick={() => selectTokenType('erc721')}></i>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default NoTokenList;
