22a79444625f0c2e89842b8e885cda6f300f7e82
dongmoon <ehdans04@gmail.com>
2021-12-17 14:11:14 +0900

    fix loading states

--- src/components/tokens/erc721/Erc721.js
+++ src/components/tokens/erc721/Erc721.js
@@ -1,15 +1,20 @@
 import { useState } from 'react';
+import { useStore } from '../../../store.js';
 import erc721Abi from '../../../erc721Abi';
 
 import Erc721SendTokenModal from './Erc721SendTokenModal';
 
 import './Erc721.css';
+import Loading from '../../loading/Loading.js';
 
 function Erc721({ web3, account, erc721list, erc721addr, setErc721list }) {
 	const [to, setTo] = useState('');
 	const [isModalOpen, setIsModalOpen] = useState(false);
 	const [selectedTokenId, setSelectedTokenId] = useState('');
-	const [isLoading, setIsLoading] = useState(false);
+	const [isLoading, setIsLoading] = useStore((state) => [
+		state.isLoading,
+		state.setIsLoading,
+	]);
 	const sendToken = async (tokenAddr, tokenId) => {
 		setIsLoading(true);
 		const tokenContract = await new web3.eth.Contract(erc721Abi, tokenAddr, {
@@ -44,7 +49,7 @@ function Erc721({ web3, account, erc721list, erc721addr, setErc721list }) {
 				const { symbol, name, tokenId, tokenURI } = token;
 				return (
 					<div className='erc721__token__card' key={tokenId}>
-						{/* TODO */}
+						{isLoading && <Loading />}
 						<div className='erc721__token__img__container'>
 							<img src={tokenURI} alt='' className='erc721__token__img' />
 						</div>
@@ -53,18 +58,6 @@ function Erc721({ web3, account, erc721list, erc721addr, setErc721list }) {
 							<h2 className='erc20__token__profile__balance'>{`id: ${tokenId}`}</h2>
 						</div>
 						<div className='erc20__token__transfer'>
-							{/* To:{' '}
-							<input
-								type='text'
-								onChange={(e) => {
-									setTo(e.target.value);
-								}}></input>
-							<button
-								className='sendErc20Btn'
-								onClick={sendToken.bind(this, erc721addr, token.tokenId)}>
-								send token
-							</button> */}
-
 							<div
 								className='token__send__button'
 								onClick={() => {
